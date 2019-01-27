e71345b78c7c73571d685e23741703d94f35385e
Saeel Parulekar <saeelmacman123@gmail.com>
2019-01-27 14:46:24 +0530

    updated post register API.

--- app.js
+++ app.js
@@ -17,6 +17,9 @@ mongoose.connect(db, {
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+//Bodyparser
+app.use(express.urlencoded({ extended: false}));
+
 // Routes
 app.use('/', require('./routes/index'));
 app.use('/users', require('./routes/users'));
--- routes/users.js
+++ routes/users.js
@@ -11,4 +11,29 @@ router.get('/register', (req, res) => res.render('register'));
 router.get('/dashboard', (req, res) => res.render('dashboard'));
 
 
+//Register handle
+router.post('/register', (req, res)=> {
+    console.log(req.body);
+
+    const {name, email, password, password2} = req.body;
+    let errors = [];
+
+    // Check required fields
+    if (!name || !email || !password || !password2) {
+        errors.push({msg: 'Please fill in all fields.'})
+    }
+    
+    //Check password match
+    if (password !== password2) {
+        errors.push({msg: 'Password do not match'})        
+    }
+
+    //check pass length
+    if (password.length < 6) {
+        errors.push({msg: 'Password should be atleast 6 character.'})                
+    }
+
+});
+
+
 module.exports = router; 
